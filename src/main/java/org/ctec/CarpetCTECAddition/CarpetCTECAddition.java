c5357f257237fc2fb33350201d2e233b226a4602
shane0411 <shane97213@gmail.com>
2024-09-27 15:10:36 +0800

    feat: change mod_name

--- src/main/java/org/ctec/CarpetCTECAddition/CarpetCTECAddition.java
+++ src/main/java/org/ctec/CarpetCTECAddition/CarpetCTECAddition.java
@@ -1,4 +1,10 @@
 package org.ctec.CarpetCTECAddition;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class CarpetCTECAddition {
+    final static String MOD_NAME = "Carpet CTEC Addition";
+    final static String MOD_ID = "carpet-ctec-addition";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 }
